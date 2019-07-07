755de3999f9993e7566714f7e6da9b354b58783e
Jake Deichert <git@jakedeichert.com>
2019-07-07 14:01:38 -0400

    Basic clap setup

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, SubCommand};
+
 fn main() {
     let machfile_contents = mach::loader::read_machfile();
     let commands = mach::parser::build_command_structure(machfile_contents);
     // dbg!(commands.clone());
 
-    println!("EXECUTING LAST COMMAND");
-    let cmd = commands.last().unwrap().clone();
-    dbg!(cmd.clone());
-    let _result = mach::executor::execute_command(cmd);
+    // println!("EXECUTING LAST COMMAND");
+    // let cmd = commands.last().unwrap().clone();
+    // dbg!(cmd.clone());
+    // let result = mach::executor::execute_command(cmd);
+
+    // dbg!(result.unwrap().clone());
+    // println!("exit code {:?}", result.unwrap().code());
+
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .multiple(true)
+                .help("Sets the level of verbosity"),
+        )
+        .get_matches();
 }
