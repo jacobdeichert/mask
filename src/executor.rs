a6d1a7f5ec81607ad2c7cac68c19438f82143807
Jake Deichert <git@jakedeichert.com>
2019-07-09 00:10:36 -0400

    Add node executor

--- src/executor.rs
+++ src/executor.rs
@@ -5,8 +5,19 @@ use std::process::ExitStatus;
 use crate::command::Command;
 
 pub fn execute_command(cmd: Command) -> Result<ExitStatus> {
-    let mut child = process::Command::new("sh");
-    child.arg("-c").arg(cmd.source);
+    let mut child = match cmd.executor.as_ref() {
+        "js" | "javascript" => {
+            let mut child = process::Command::new("node");
+            child.arg("-e").arg(cmd.source);
+            child
+        }
+        _ => {
+            let mut child = process::Command::new("sh");
+            child.arg("-c").arg(cmd.source);
+            child
+        }
+    };
+
 
     // Add all required args as environment variables
     for arg in cmd.required_args {
