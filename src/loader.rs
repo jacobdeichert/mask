cbafb8630b99998fe849a0e17f8deabe0780f94b
Jake Deichert <git@jakedeichert.com>
2019-07-13 15:19:18 -0400

    Search for a maskfile.md first

--- src/loader.rs
+++ src/loader.rs
@@ -3,8 +3,13 @@ use std::io::prelude::*;
 
 pub fn read_maskfile() -> String {
     // TODO: try to find maskfile in current directory and maybe parent directories?
-    // TODO: search for variations: maskfile, maskfile.md, Maskfile.md (case insensitive)
-    let mut file = File::open("maskfile").expect("expected maskfile to exist");
+    let file = File::open("maskfile.md").or(File::open("maskfile"));
+
+    if file.is_err() {
+        panic!("Expected a maskfile(.md) to exist in the current directory.")
+    }
+
+    let mut file = file.unwrap();
     let mut maskfile_contents = String::new();
     file.read_to_string(&mut maskfile_contents)
         .expect("expected file contents");
