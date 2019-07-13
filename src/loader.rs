067a03b1bbc27f1e710cfa9ebd863de26885ba89
Jake Deichert <git@jakedeichert.com>
2019-07-13 13:58:29 -0400

    Create a maskfile for the development of mask itself

--- src/loader.rs
+++ src/loader.rs
@@ -4,7 +4,7 @@ use std::io::prelude::*;
 pub fn read_maskfile() -> String {
     // TODO: try to find maskfile in current directory and maybe parent directories?
     // TODO: search for variations: maskfile, maskfile.md, Maskfile.md (case insensitive)
-    let mut file = File::open("test/maskfile").expect("expected maskfile to exist");
+    let mut file = File::open("maskfile").expect("expected maskfile to exist");
     let mut maskfile_contents = String::new();
     file.read_to_string(&mut maskfile_contents)
         .expect("expected file contents");
