52c3d16650c8122e5fc3a93983ca042222793805
Jake Deichert <git@jakedeichert.com>
2019-07-24 22:57:27 -0400

    Add tests for exit status code

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,35 +1,85 @@
-use assert_cmd::crate_name;
 use assert_cmd::prelude::*;
-use assert_fs::prelude::*;
-use std::process::Command;
+use predicates::str::contains;
+
+mod common;
+use common::MaskCommandExt;
 
 #[test]
 fn specifying_a_maskfile_in_a_different_dir() {
-    let temp = assert_fs::TempDir::new().unwrap();
-    let maskfile = temp.child("maskfile.md");
-
-    let src = "
-# Integration tests
-
-> A line describing the integration tests
-
+    let (_temp, maskfile_path) = common::maskfile(
+        r#"
 ## foo
 
 <!-- a few more details -->
+"#,
+    );
 
-## bar
-
-<!-- a few more details -->
-";
-
-    maskfile.write_str(src).unwrap();
-
-    let maskfile_path = maskfile.path().to_str().unwrap();
-
-    let mut mask = Command::cargo_bin(crate_name!()).unwrap();
-    mask.arg("--maskfile")
+    common::run_mask(&maskfile_path)
+        .arg("--maskfile")
         .arg(maskfile_path)
         .arg("--help")
         .assert()
+        .stdout(contains("USAGE:"))
         .success();
+}
+
+mod exits_with_the_child_process_status_code {
+    use super::*;
+
+    #[test]
+    fn exits_with_success() {
+        let (_temp, maskfile_path) = common::maskfile(
+            r#"
+## success
+
+~~~sh
+exit 0
+~~~
+"#,
+        );
+
+        common::run_mask(&maskfile_path)
+            .command("success")
+            .assert()
+            .code(0)
+            .success();
+    }
+
+    #[test]
+    fn exits_with_error1() {
+        let (_temp, maskfile_path) = common::maskfile(
+            r#"
+## failure
+
+~~~sh
+exit 1
+~~~
+"#,
+        );
+
+        common::run_mask(&maskfile_path)
+            .command("failure")
+            .assert()
+            .code(1)
+            .failure();
+    }
+
+    #[test]
+    fn exits_with_error2() {
+        let (_temp, maskfile_path) = common::maskfile(
+            r#"
+## failure
+
+~~~sh
+exit 2
+~~~
+"#,
+        );
+
+        common::run_mask(&maskfile_path)
+            .command("failure")
+            .assert()
+            .code(2)
+            .failure();
+    }
 }
